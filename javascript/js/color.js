63f702e0228d3e0762117b59fb6e8ad1e6feb432
Zeus21z <166827702+Zeus21z@users.noreply.github.com>
2024-04-17 08:08:50 -0400

    Add files via upload

---
+++ javascript/js/color.js
@@ -0,0 +1,15 @@
+//COMENTARIO DE UNA LINEA
+/*
+Comentarios
+multiples
+lineas
+*/
+/*
+Descripcion> Ejemplo de web de color
+Desarrollador Zeus Villarroel
+Fecha 2024-04-15
+Cambios Ninguno
+*/
+var color = prompt("Por favor, ingresa un color:");
+
+console.log("El color ingresado es: " + color);
