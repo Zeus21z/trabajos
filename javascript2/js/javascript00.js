fd1726a32a53969268acf3af23d7b6e20fdfde39
Zeus21z <166827702+Zeus21z@users.noreply.github.com>
2024-04-22 09:52:23 -0400

    Add files via upload

---
+++ javascript2/js/javascript00.js
@@ -0,0 +1,13 @@
+//COMENTARIO DE UNA LINEA
+/*
+Comentarios
+multiples
+lineas
+*/
+/*
+Descripcion> Ejemplo de Hola mundo por console.log.
+Desarrollador Nombre y Apellido
+Fecha 2024-04-15
+Cambios Ninguno
+*/
+console.log("Hola mundo INTERNO");
---
+++ javascript2/js/javascript01.js
@@ -0,0 +1,14 @@
+//COMENTARIO DE UNA LINEA
+/*
+Comentarios
+multiples
+lineas
+*/
+/*
+Descripcion> Ejemplo de Hola mundo por console.log.
+Desarrollador Nombre y Apellido
+Fecha 2024-04-15
+Cambios Ninguno
+*/
+var mensaje="Imprimir en Consola";
+console.log("mensaje");
